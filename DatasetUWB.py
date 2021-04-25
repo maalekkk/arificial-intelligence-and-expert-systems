eb5b1c7500750a29341bdcf6d53d5b199e800b6c
Bartlomiej Malkowski <215817@edu.p.lodz.pl>
2021-04-25 21:39:31 +0200

    add class to import datasets

--- DatasetUWB.py
+++ DatasetUWB.py
@@ -2,28 +2,26 @@ import pandas as pd
 
 
 class DatasetUWD:
-
     def __init__(self, audience_no):
         self.audience_no = audience_no
-        self.x = []
-        self.y = []
-        self.z = []
+        self.coords = []
+        self.reference = []
 
-    def __len__(self):
-        return len(self.x)
+    def import_file(self, file_name):
+        data = pd.read_excel(file_name)
+        data = pd.DataFrame(data,
+                            columns=["data__coordinates__x", "data__coordinates__y", "reference__x", "reference__y"])
+        file_coords = []
+        file_reference = []
+        for i in range(len(data)):
+            row = data.iloc[i]
+            file_coords.append([row[0], row[1]])
+            file_reference.append([row[2], row[3]])
+        self.coords.append(file_coords)
+        self.reference.append(file_reference)
 
-    def __getitem__(self, index):
-        return [self.x[index], self.y[index], self.z[index]]
-
-    def import_file(self, file_no):
-        file_path = "./dane/pomiary/F" + str(self.audience_no) + "/f" + str(self.audience_no) + "_stat_" + str(
-            file_no) + ".xlsx"
-        data = pd.read_excel(file_path)
-        data = pd.DataFrame(data, columns=["data__coordinates__x", "data__coordinates__y", "data__coordinates__z"])
-        self.x.append(data["data__coordinates__x"].tolist())
-        self.y.append(data["data__coordinates__y"].tolist())
-        self.z.append(data["data__coordinates__z"].tolist())
-
-    def import_data(self):
+    def import_static_data(self):
         for i in range(1, 225):
-            self.import_file(i)
+            file_path = "./dane/pomiary/F" + str(self.audience_no) + "/f" + str(self.audience_no) + "_stat_" + str(
+                i) + ".xlsx"
+            self.import_file(file_path)
