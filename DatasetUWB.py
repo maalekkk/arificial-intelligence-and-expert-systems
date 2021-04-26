f3bf828c3115c6515512272857c465699be97375
Bartlomiej Malkowski <215817@edu.p.lodz.pl>
2021-04-26 14:35:25 +0200

    fix dataset class to torch

--- DatasetUWB.py
+++ DatasetUWB.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import torch
+import numpy as np
 
 
 class DatasetUWD:
@@ -11,17 +13,26 @@ class DatasetUWD:
         data = pd.read_excel(file_name)
         data = pd.DataFrame(data,
                             columns=["data__coordinates__x", "data__coordinates__y", "reference__x", "reference__y"])
-        file_coords = []
-        file_reference = []
+
         for i in range(len(data)):
             row = data.iloc[i]
-            file_coords.append([row[0], row[1]])
-            file_reference.append([row[2], row[3]])
-        self.coords.append(file_coords)
-        self.reference.append(file_reference)
+            self.coords.append([row[0], row[1]])
+            self.reference.append([row[2], row[3]])
 
     def import_static_data(self):
-        for i in range(1, 225):
+        for i in range(1, 25):
             file_path = "./dane/pomiary/F" + str(self.audience_no) + "/f" + str(self.audience_no) + "_stat_" + str(
                 i) + ".xlsx"
             self.import_file(file_path)
+
+    def get_torch_dataset(self):
+        return torch.from_numpy(np.array(self.coords).astype(np.float32)), torch.from_numpy(
+            np.array(self.reference).astype(np.float32))
+
+
+if __name__ == '__main__':
+    d = DatasetUWD(8)
+    d.import_file("./dane/pomiary/F8/f8_stat_1.xlsx")
+    c, r = d.get_torch_dataset()
+    print(c)
+    print(r)
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,20 +1,70 @@
-from torch import nn
+from torch import nn, optim
+
+import DatasetUWB
 
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(2, 10),
-            nn.ReLU(),
-            nn.Linear(10, 10),
-            nn.ReLU(),
-            nn.Linear(10, 2),
-            nn.ReLU()
-        )
+        self.fc1 = nn.Linear(2, 10)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(10, 6)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(6, 2)
+
+    # def __init__(self):
+    #     super(NeuralNetwork, self).__init__()
+    #     self.flatten = nn.Flatten()
+    #     self.linear_relu_stack = nn.Sequential(
+    #         nn.Linear(2, 10),
+    #         nn.ReLU(),
+    #         nn.Linear(10, 10),
+    #         nn.ReLU(),
+    #         nn.Linear(10, 2),
+    #         nn.ReLU()
+    #     )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        out = self.fc1(x)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        out = self.relu2(out)
+        out = self.fc3(out)
+        return out
+
+    # def forward(self, x):
+    #     x = self.flatten(x)
+    #     logits = self.linear_relu_stack(x)
+    #     return logits
+
+    def perform_training(self, epochs, training_data, reference_data):
+        assert(epochs > 0)
+        assert(len(training_data) == len(reference_data))
+
+        criterion = nn.L1Loss()
+        optimizer = optim.SGD(self.parameters(), lr=0.05)
+
+        for epoch in range(epochs):
+            running_loss = 0
+
+            for _ in range(len(training_data)):
+                optimizer.zero_grad()
+                output = self(training_data)
+                loss = criterion(output, reference_data)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+            print(running_loss / len(training_data))
+
+
+if __name__ == '__main__':
+    dataset = DatasetUWB.DatasetUWD(8)
+    dataset.import_static_data()
+    c, r = dataset.get_torch_dataset()
+    model = NeuralNetwork()
+
+    print(model)
+    model.perform_training(100, c, r)
+
